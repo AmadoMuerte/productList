b3763ed968b092c5f9a2a8f1b0ad01569dfb794a
AmadoMuerte <kisik854@gmail.com>
2022-01-07 21:54:55 +1000

    create product list

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+/* создать лист с продуктами
+   сделать функцию вывода текста в инпутах  и добавления этого в лист продуктов 
+    */
+
+
+let productList = {};
+
+const prodName = document.querySelector('.input__text'),
+      count = document.querySelector('.input__count'),
+      countTitle = document.querySelector('.counts'),
+      addBtn = document.querySelector('.btn-add'),
+      calcBtn = document.querySelector('.btn-count'),
+      list = document.querySelector('.list');
+
+addBtn.addEventListener('click', () => {
+    outputProduct();
+    addProductList(prodName, count);
+    console.log(productList);
+});
+
+calcBtn.addEventListener('click', () => {
+    let sum = calcProduct(productList);
+    countTitle.innerText = `Сумма товаров - ${sum} руб`;
+});
+
+
+
+function addProductList(product, count) {
+
+    if (prodName.value != '' && count.value != '') {
+        productList[product.value] = +count.value;
+        return;
+    }
+}
+
+function outputProduct() {
+
+    if (prodName.value != '' && count.value != '') {
+        let li = document.createElement('li');
+        li.classList.add('list__item');
+        li.prepend(`${prodName.value} - ${count.value} руб`);
+        list.prepend(li);
+   }
+
+}
+
+function calcProduct(obj) {
+
+        let sum = 0;
+
+        for( let el in obj ) {
+          if( obj.hasOwnProperty( el ) ) {
+            sum += parseFloat( obj[el] );
+          }
+        }
+        
+        return sum;
+
+}
