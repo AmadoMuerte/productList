f41f65f6100b77187b1c82d4821a00ed06258645
AmadoMuerte <kisik854@gmail.com>
2022-01-11 22:05:13 +1000

    create delete btn

--- js/script.js
+++ js/script.js
@@ -13,19 +13,35 @@ const prodName = document.querySelector('.input__text'),
       calcBtn = document.querySelector('.btn-count'),
       list = document.querySelector('.list');
 
+
 addBtn.addEventListener('click', () => {
-    outputProduct();
+
+    const li = outputProduct();
+
     addProductList(prodName, count);
+
+    const BtnDelete = document.createElement('span');
+    BtnDelete.classList.add('list__item-close');
+    BtnDelete.innerText = '✘';
+    li.append(BtnDelete);
+
+    BtnDelete.addEventListener('click', () => {
+    
+        li.remove();
+    
+    });
     console.log(productList);
+
 });
 
 calcBtn.addEventListener('click', () => {
+
     let sum = calcProduct(productList);
     if (sum != 0) {
         countTitle.innerText = `Сумма товаров - ${sum} руб`;
     }
-});
 
+});
 
 
 function addProductList(product, count) {
@@ -36,27 +52,29 @@ function addProductList(product, count) {
     }
 }
 
+
 function outputProduct() {
 
     if (prodName.value != '' && count.value != '') {
-        let li = document.createElement('li');
+        const li = document.createElement('li');
+        
         li.classList.add('list__item');
-        li.prepend(`${prodName.value} - ${count.value} руб`);
+        li.prepend(`${prodName.value} - ${count.value} руб`);   
         list.prepend(li);
+        return li;
    }
 
 }
 
+
 function calcProduct(obj) {
 
         let sum = 0;
-
         for( let el in obj ) {
           if( obj.hasOwnProperty( el ) ) {
             sum += parseFloat( obj[el] );
           }
         }
-        
         return sum;
 
 }
